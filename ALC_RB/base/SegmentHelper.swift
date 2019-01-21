00bec8b4facbc78c3b943b9e3d22ec3440fcfd8d
Ayur Arkhipov <gangrue@gmail.com>
2019-01-21 12:21:37 +0800

    from home migrate 21.01.2019

---
+++ ALC_RB/base/SegmentHelper.swift
@@ -0,0 +1,34 @@
+//
+//  SegmentHelper.swift
+//  ALC_RB
+//
+//  Created by ayur on 18.01.2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Using with view container and segmented control
+class SegmentHelper {
+    
+    var rootViewController: UIViewController?
+    var rootViewContainer: UIView?
+    
+    init(_ root: UIViewController, _ view: UIView) {
+        self.rootViewController = root
+        self.rootViewContainer = view
+    }
+    
+    func add(_ viewController: UIViewController) {
+        rootViewController?.addChild(viewController)
+        rootViewContainer?.addSubview(viewController.view)
+        viewController.view.frame = (rootViewContainer?.bounds)!
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    func remove(_ viewController: UIViewController) {
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+}
--- ALC_RB/ui/tournaments/detail/teams/detail/TeamLeagueDetailViewController.swift
+++ ALC_RB/ui/tournaments/detail/teams/detail/TeamLeagueDetailViewController.swift
@@ -10,7 +10,57 @@ import UIKit
 
 class TeamLeagueDetailViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var viewContainer: UIView!
+    
+    private lazy var playersTable: PlayersTeamLeagueDetailViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "PlayersTeamLeagueDetailViewController") as! PlayersTeamLeagueDetailViewController
+        
+        viewController.leagueDetailModel = self.leagueDetailModel
+        
+        return viewController
+    }()
+    private lazy var matchesTable: MatchesTeamLeagueDetailTableViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "MatchesTeamLeagueDetailTableViewController") as! MatchesTeamLeagueDetailTableViewController
+        
+        viewController.leagueDetailModel = self.leagueDetailModel
+        
+        return viewController
+    }()
+    var segmentHelper: SegmentHelper?
+    
+    var leagueDetailModel = LeagueDetailModel()
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            segmentHelper?.remove(matchesTable)
+            segmentHelper?.add(playersTable)
+            break
+        case 1:
+            segmentHelper?.remove(playersTable)
+            segmentHelper?.add(matchesTable)
+            break
+        default:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
+    }
+    
+    func initView() {
+        segmentHelper = SegmentHelper(self, viewContainer)
+        firstSegmentInit()
+    }
+    
+    func firstSegmentInit() {
+        segmentHelper?.add(playersTable)
     }
 }
