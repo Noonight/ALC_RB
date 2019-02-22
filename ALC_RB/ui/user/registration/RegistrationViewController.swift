40622dfe86df62965213f6f8bf9b85579722f4f4
Ayur Arkhipov <gangrue@gmail.com>
2019-02-22 13:41:06 +0800

    (fix) authorisation, registration view

--- ALC_RB/ui/user/registration/RegistrationViewController.swift
+++ ALC_RB/ui/user/registration/RegistrationViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    // MARK: - Variables
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var familyTextField: UITextField!
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var patronymicTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var birthdayPicker: UIDatePicker!
+    @IBOutlet weak var barCompleteButton: UIBarButtonItem!
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,9 +31,15 @@ class RegistrationViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         navigationController?.isNavigationBarHidden = false
+        barCompleteButton.image =  barCompleteButton.image?.af_imageAspectScaled(toFit: CGSize(width: 22, height: 22))
     }
     
-
+    // MARK: - Actions
+    
+    @IBAction func completeRegistration(_ sender: UIBarButtonItem) {
+        print("complete registration button pressed")
+    }
+    
     /*
     // MARK: - Navigation
 
