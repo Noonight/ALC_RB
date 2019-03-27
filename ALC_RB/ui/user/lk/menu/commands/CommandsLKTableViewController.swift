1b8a624342d9c636391bb5ee41f40ff256361931
Ayur Arkhipov <gangrue@gmail.com>
2019-03-28 03:07:32 +0800

    (mock) in development

--- ALC_RB/ui/user/lk/menu/commands/CommandsLKTableViewController.swift
+++ ALC_RB/ui/user/lk/menu/commands/CommandsLKTableViewController.swift
@@ -268,6 +268,8 @@ class CommandsLKTableViewController: BaseStateTableViewController {
         default:
             break
         }
+        
+        cell.selectionStyle = .none
 
         return cell
     }
