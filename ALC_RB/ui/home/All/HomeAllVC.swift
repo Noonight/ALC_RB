78dbce025b223ace9b67f42d2ceb27cf2132b823
Ayur <gangrue@gmail.com>
2019-08-19 16:55:04 +0800

    (add) home view start

---
+++ ALC_RB/ui/home/All/HomeAllVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeAllVC.swift
+//  ALC_RB
+//
+//  Created by Ayur Arkhipov on 19/08/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class HomeAllVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
